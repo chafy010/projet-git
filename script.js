9bd782e7f0ad8ca0a3ad1ac8d5afe902ce11332b
tanga <salafy010@gmail.com>
2024-12-21 17:37:22 +0100

    ajouter des fichiers

---
+++ script.js
@@ -0,0 +1,9 @@
+let nom="ambela";
+let age=12;
+let adress="rabat hirfane";
+let note=14;
+if(note>13){
+    console.log("vous meitez un tableau d'honneur");
+}else{
+    console.log("vous recevez le bulletin seulement");
+}
